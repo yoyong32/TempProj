f5ec4eb84f04d92f95b881e2e0cc71ac80b16748
Yong Tae Kwon <ykwon14@umd.edu>
2022-07-31 00:26:39 -0400

    Completed Cell

---
+++ web/src/components/AppsCell/AppsCell.tsx
@@ -0,0 +1,32 @@
+import type { AppsQuery } from 'types/graphql'
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+
+
+export const QUERY = gql`
+  query AppsQuery {
+    apps: applications {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ apps }: CellSuccessProps<AppsQuery>) => {
+  return (
+    <ul>
+      {apps.map((item) => {
+        return <li key={item.id}>{JSON.stringify(item)}</li>
+      })}
+    </ul>
+  )
+}
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import { MetaTags } from '@redwoodjs/web'
+import AppsCell from 'src/components/AppsCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
+      <AppsCell/>
     </>
   )
 }
